8c8a51b1ecedc393c6e0b0515102f2bd8b270412
Bhanu Agrawal <bhanu.agrawal@dechea.com>
2021-11-22 19:24:24 +0530

    done

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
-    res.status(200).json({
-      body: req.body,
-      query: req.query,
-      cookies: req.cookies,
-    });
+    // res.status(200).json({
+    //   body: req.body,
+    //   query: req.query,
+    //   cookies: req.cookies,
+    // });
+    res.send("Hello World")
   }
